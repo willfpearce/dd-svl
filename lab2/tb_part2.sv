ae32fea59a7c463f91faa6f0faba801d8f867932
Will Pearce <william.pearce@numonix.io>
2025-12-03 17:20:27 -0500

    a

--- lab2/tb_part2.sv
+++ lab2/tb_part2.sv
@@ -100,6 +100,10 @@ localparam int PROG_LEN =100;
       bins b [] = {[0:255]};
       
     }
+    
+    // (2a) Cross coverage for operands a and b
+    cross a_sig, b_sig;
+    
     coverpoint opcode_sig {
       bins opcode []= {[0:15]};
       
