4a7528c5be9490dcde4a6b614910ca05c274496d
Will Pearce <william.pearce@numonix.io>
2025-12-03 17:38:54 -0500

    a

--- lab2/tb_lab3.sv
+++ lab2/tb_lab3.sv
@@ -59,7 +59,10 @@ localparam int PROG_LEN =100;
 
 
 // Comment (2c) Add Contraint 2 here: Using the implication operator ->, define a constraint named small_operands 
-// which will constrain, “If a and b are both small (<25), then the opcode is changed to ADD”.
+// which will constrain, "If a and b are both small (<25), then the opcode is changed to ADD".
+    constraint small_operands {
+      (a < 25 && b < 25) -> (opcode == 4'b0001);
+    }
 
 
 
